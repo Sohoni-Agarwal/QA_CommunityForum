300f77a40cf48a01695a5d7f4e136395e5850805
Sohoni Agarwal <sohoniaggarwal13@gmail.com>
2019-09-26 21:17:00 +0530

    First commit

---
+++ admins/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+
+#def index(request):
+  #  return render(request, 'frontend/templates/index.html')
+
+
+class HomePageView(TemplateView):
+    template_name = 'login.html'
